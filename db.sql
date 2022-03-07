45ca90d707301f71df68c8aafa0c08dd8cc5ddb8
Fotis Alexandridis <eleft@protonmail.com>
2022-03-07 13:07:35 +0200

    set up dependencies

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS metaspacewars;
+USE metaspacewars;
+
+CREATE TABLE IF NOT EXISTS player (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    pubkey VARCHAR(50) NOT NULL,
+    metadata TEXT
+);
