d689193335da8fb0d0ce73f7e257c66b4e094d4a
Fotis Alexandridis <eleft@protonmail.com>
2022-03-12 21:49:45 +0000

    structure added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
-// if we ever need a server connection to validate things
-// app.get('/', (req, res) => {
-//   res.send("hello!");
-// });
-
-// user has connected
+// new user connection
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log("New user connected");
+
+  // TODO: game start listener
+  socket.on("game start", (data) => {
+
+  });
+
+  // TODO: enemy kill listener
+  socket.on("enemy kill", (data) => {
+
+  });
+  
+  // TODO: wave clear listener
+  socket.on("wave clear", (data) => {
+
+  });
+
+  // TODO: game end listener
+  socket.on("game end", (data) => {
+
+  });
 });
 
-server.listen(3000, () => {
+// start the server on $PORT, defaults to 3000
+server.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
