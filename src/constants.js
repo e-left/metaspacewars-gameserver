363f884f47d75f49bf077053af7d85e4f69d9a84
Fotis Alexandridis <eleft@protonmail.com>
2022-03-14 13:51:59 +0200

    Core anticheat functionality implemented

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,7 @@
 const EnemiesPerWave = 20;
 const WavesPerLive = 5;
+const PointsPerEnemy = 100;
+const PointsPerWave = 1000;
+const LivesPointMultiplier = 1.5;
 
-module.exports = {EnemiesPerWave, WavesPerLive};
+module.exports = {EnemiesPerWave, WavesPerLive, PointsPerEnemy, PointsPerWave, LivesPointMultiplier};
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const { Server } = require("socket.io");
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 
-const {createPlayer, getPlayers, readPlayer, updatePlayer, deletePlayer, getLeaderboard, getRanking, getCount} = require('./data.js');
+const { createPlayer, getPlayers, readPlayer, updatePlayer, deletePlayer, getLeaderboard, getRanking, getCount } = require('./data.js');
+const { EnemiesPerWave, WavesPerLive, PointsPerEnemy, PointsPerWave, LivesPointMultiplier } = require('./constants');
 
 const app = express();
 app.use(cors());
@@ -16,22 +17,29 @@ const io = new Server(server);
 const inMemoryPlayers = {};
 
 // new user connection
+// TODO: save in metadata the skin selection, make sure to validate it
 io.on('connection', (socket) => {
   console.log("New user connected");
 
-  // TODO: game start listener
+  // game start listener
   socket.on("game start", (data) => {
     // data = {
     //   "pubkey"
     // }
+    const pubkey = data.pubkey;
 
     // creates a new game IN MEMORY to be tracked
     // through a global hashmap (object)
     const gameID = uuidv4();
     let player = {
-      pubkey: data.pubkey,
+      pubkey: pubkey,
       highscore: 0,
-      metadata: {},
+      metadata: {
+        enemiesKilled: 0,
+        wave: 1,
+        lives: 1,
+        cheat: false,
+      },
     };
 
     inMemoryPlayers[gameID] = player;
@@ -44,77 +52,176 @@ io.on('connection', (socket) => {
       pubkey: player.pubkey,
       gameID: gameID
     };
-  
+
   });
 
-  // TODO: enemy kill listener
+  // enemy kill listener
   socket.on("enemy kill", (data) => {
     // data = {
     //   "pubkey",
     //   "gameID"
     // }
+    const pubkey = data.pubkey;
+    const gameID = data.gameID;
+    let done = false;
 
-    // ensure here proper rate limiting and wave limits
+    // ensure here proper wave limits
+    // TODO: maybe add rate limits?
+    let valid = false;
+    let currentWaveEnemiesKilled = inMemoryPlayers[gameID].metadata.enemiesKilled - EnemiesPerWave * (inMemoryPlayers[gameID].metadata.wave - 1);
+
+    if (currentWaveEnemiesKilled <= EnemiesPerWave && inMemoryPlayers[gameID].metadata.lives > 0) {
+      valid = true;
+    } else {
+      inMemoryPlayers[gameID].metadata.cheat = true;
+    }
 
     // add enemy kill
+    if (valid && inMemoryPlayers[gameID].pubkey === pubkey) {
+      inMemoryPlayers[gameID].metadata.enemiesKilled += 1;
+      done = true;
+      // add score according to calculation here
+      inMemoryPlayers[gameID].highscore += PointsPerEnemy;
+    };
 
     // returnedObject = {
     //   "pubkey",
-    //   "gameID"
+    //   "gameID",
+    //   "score",
+    //   "valid",
+    //   "done"
     // }
-
+    return {
+      pubkey: pubkey,
+      gameID: gameID,
+      score: inMemoryPlayers[gameID].highscore,
+      valid: valid,
+      done: done,
+    };
   });
-  
-  // TODO: wave clear listener
+
+  // wave clear listener
   socket.on("wave clear", (data) => {
     // data = {
     //   "pubKey",
     //   "gameID"
     // }
+    const pubkey = data.pubkey;
+    const gameID = data.gameID;
+    let done = false;
 
     // ensure here wave limits of enemies
+    // TODO: maybe add rate limits?
+    let valid = false;
+    let currentWaveEnemiesKilled = inMemoryPlayers[gameID].metadata.enemiesKilled - EnemiesPerWave * (inMemoryPlayers[gameID].metadata.wave - 1);
+
+    if (currentWaveEnemiesKilled === EnemiesPerWave && inMemoryPlayers[gameID].metadata.lives > 0) {
+      valid = true;
+    } else {
+      inMemoryPlayers[gameID].metadata.cheat = true;
+    }
 
     // add wave clear with lives
+    if (valid && inMemoryPlayers[gameID].pubkey === pubkey) {
+      inMemoryPlayers[gameID].metadata.wave += 1;
+      done = true;
+      // add score according to calculation here
+      inMemoryPlayers[gameID].highscore += PointsPerWave + PointsPerWave * (inMemoryPlayers[gameID].metadata.lives - 1) * (LivesPointMultiplier - 1);
+
+      // add live if in specified wave
+      if (inMemoryPlayers[gameID].metadata.wave % WavesPerLive === 0) {
+        inMemoryPlayers[gameID].metadata.lives += 1;
+      }
+    };
+
 
     // returnedObject = {
-    //   "pubKey",
-    //   "gameID"
+    //   "pubkey",
+    //   "gameID",
+    //   "score",
+    //   "valid",
+    //   "done"
     // }
+    return {
+      pubkey: pubkey,
+      gameID: gameID,
+      score: inMemoryPlayers[gameID].highscore,
+      valid: valid,
+      done: done,
+    };
 
   });
 
-  // TODO: game end listener
-  socket.on("game end", (data) => {
+  // game end listener
+  socket.on("game end", async (data) => {
     // data = {
     //   "pubKey",
     //   "gameID"
     // }
+    const pubkey = data.pubkey;
+    const gameID = data.gameID;
+    let done = false;
+    let valid = false;
 
     // ensure valid game
+    // ensure lives are zero
+    // ensure no cheating
+    // ensure enemies are less than (or equal, edge case with fired bullet) wave enemies (since we die mid-wave)
+    if(inMemoryPlayers[gameID].metadata.lives === 0 && !inMemoryPlayers.metadata.cheat && (inMemoryPlayers[gameID].metadata.enemiesKilled - (EnemiesPerWave * inMemoryPlayers[gameID].metadata.wave - 1)) <= EnemiesPerWave) {
+      valid = true;
+    }
 
     // write to database
+    // TODO: save in metadata skins used
+    // no new validation needed, since it was validated upon game creation
+    let newEntry;
+    if(valid) {
+      newEntry = await createPlayer(inMemoryPlayers[gameID]);
+      done = true;
+    }
+
+    // delete the in memory entry
+    delete inMemoryPlayers[gameID];
 
     // returnedObject = {
     //   "pubKey",
-    //   "gameID"
+    //   "gameID",
+    //   "score",
+    //   "valid",
+    //   "done"
     // }
-
+    return {
+      pubkey: pubkey,
+      gameID: gameID,
+      score: done ? newEntry.highscore : 0,
+      valid: valid,
+      done: done,
+    };
   });
 });
 
 // handler for the leaderboard to be returned
-// TODO: implement this
-app.get("/leaderboard", (req, res) => {
+app.get("/leaderboard/:count/:offset", async (req, res) => {
   // get leaderboard as JSON
+  const count = req.query.count;
+  const offset = req.query.offset;
+
+  const leaderboard = await getLeaderboard(count, offset);
+  if (leaderboard.error) {
+    res.status(400).send("Invalid offset!");
+  }
 
   // return the JSON
-
+  res.status(200).json(leaderboard);
 });
 
 // handler for user statistics to be returned
-// TODO: implement this
-app.get("/user/:id", (req, res) => {
+app.get("/player/:id", async (req, res) => {
+  const id = req.query.id;
 
+  const player = await readPlayer(id);
+
+  res.status(200).json(player);
 });
 
 // start the server on $PORT, defaults to 3000
